23b52d5cd9bbbe2b987248695f7845b10cd8c53a
Mads Opheim <mads.opheim@gmail.com>
2023-04-13 13:20:04 +0200

    Convert from Java to Kotlin

---
+++ src/main/kotlin/no/madsopheim/enterpriseKotlin/Application.kt
@@ -0,0 +1,7 @@
+package no.madsopheim.enterpriseKotlin
+
+import jakarta.ws.rs.ApplicationPath
+import jakarta.ws.rs.core.Application
+
+@ApplicationPath("/api")
+open class Application : Application()
