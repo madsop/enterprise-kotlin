3895fda7cdc68847874420eff4caaa9a13c9d7cf
Mads Opheim <mads.opheim@gmail.com>
2023-04-13 13:43:50 +0200

    Introducing Jakarta Validation

--- src/main/kotlin/no/madsopheim/enterpriseKotlin/Paths.kt
+++ src/main/kotlin/no/madsopheim/enterpriseKotlin/Paths.kt
@@ -1,9 +1,11 @@
 package no.madsopheim.enterpriseKotlin
 
 import jakarta.enterprise.context.ApplicationScoped
+import jakarta.validation.constraints.NotEmpty
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
+import jakarta.ws.rs.QueryParam
 import jakarta.ws.rs.core.MediaType
 
 @Path("/paths")
@@ -13,7 +15,11 @@ open class Paths {
     @Path("/hello")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    open fun hello() = HelloResponse("Hello, Kotlin")
+    open fun hello(
+        @NotEmpty
+        @QueryParam("name")
+        name: String,
+    ) = HelloResponse("Hello, from Kotlin to $name")
 }
 
 data class HelloResponse(val text: String)
