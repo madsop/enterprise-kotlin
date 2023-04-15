221da5ab09031933af1bf695d64cc7321d895bd5
Mads Opheim <mads.opheim@gmail.com>
2023-04-15 15:36:09 +0200

    Introducing Jakarta Persistence

--- src/main/kotlin/no/madsopheim/enterpriseKotlin/Paths.kt
+++ src/main/kotlin/no/madsopheim/enterpriseKotlin/Paths.kt
@@ -2,30 +2,37 @@ package no.madsopheim.enterpriseKotlin
 
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.inject.Inject
+import jakarta.transaction.Transactional
 import jakarta.validation.constraints.NotEmpty
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.QueryParam
 import jakarta.ws.rs.core.MediaType
+import kotlin.random.Random
 
 @Path("/paths")
 @ApplicationScoped
-class Paths(@Inject var repository: Repository) {
+class Paths(@Inject private var repository: Repository) {
 
     @Path("/hello")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
     fun hello(
         @NotEmpty
         @QueryParam("name")
         name: String,
-    ) = HelloResponse("Hello, from ${repository.language()} to $name")
+    ): HelloResponse {
+        val id = Random.nextInt()
+        repository.create(Hello(id, "the repository ${System.currentTimeMillis()}"))
+        return HelloResponse("Hello, from ${repository.read(id)} to $name")
+    }
 
     @Path("/simpleHello")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    fun hello2() = HelloResponse("Hello, from Kotlin")
+    fun simpleHello() = HelloResponse("Hello, from Kotlin")
 }
 
 data class HelloResponse(val text: String)
